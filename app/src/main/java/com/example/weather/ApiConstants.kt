3f5142a0ae53e71fb64b9b8f806fd3d15f924f91
ShkiTTer <shkipper65@gmail.com>
2019-07-17 13:05:23 +0700

    Change ApiConstants

--- app/src/main/java/com/example/weather/ApiConstants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.weather
-
-object ApiConstants {
-    const val WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val WEATHER_UNITS = "metric"
-    const val WEATHER_API = "2fabe55b56feaaa7a007e467f6c4a105"
-}
